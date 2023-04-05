0544591d2ee735f932c53cf1a4ffb13782d70096
celadyn <dsrichmond@aol.com>
2023-04-05 18:08:33 -0500

    added total page calc

--- Powershell/ChatGPT testing.ps1
+++ Powershell/ChatGPT testing.ps1
@@ -1,1 +1,2 @@
-Get-OpenAIUsage
+Get-Item function:Get-MgGraphAllPages | select -expand definition | ai "Generate a comment-based help block for this powershell function. Start with <# and end with #>. Do not modify the function or explain anything outside of the comment-based help block."
+
